a2fb77bbdaf039f8c41428b5ef0c5dc54f7f6ab5
SSABI <fhy1930@gmail.com>
2020-12-13 22:52:13 +0900

    add Permutation.java

---
+++ Permutation.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Permutation{
+    private int n;
+    private int r;
+    private int[] now;
+    private ArrayList<ArrayList<Integer>> result;
+
+    public Permutation(int n, int r){
+        this.n = n;
+        this.r = r;
+        this.now = new int[r];
+        this.result = new ArrayList<ArrayList<Integer>>();
+    }
+
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void permutation(int[] arr, int depth){
+        if(depth == r){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int i=0; i<now.length; i++){
+                temp.add(now[i]);
+            }
+            result.add(temp);
+            return;
+        }
+
+        for(int i=depth; i<n; i++){
+            swap(arr, i, depth);
+            now[depth] = arr[depth];
+            permutation(arr, depth + 1);
+            swap(arr, i, depth);
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> getResult(){
+        return result;
+    }
+}
